bd04e55b30e0976b753261e10bcdd550738fab45
Yrmih Ian <yrmihiangoncalvesdelima@gmail.com>
2025-08-28 12:07:19 -0300

    feat-criando variaveis de ambiente com o dotenv pois não funcionou dentro de export default devido ao escopo de variaveis

--- config/default.ts
+++ config/default.ts
@@ -1,9 +1,10 @@
+const  dbUser = process.env.DB_USER;
+const  dbPass = process.env.DB_PASS;
+
 
 export default {
   port: 3000,
-  dbUser: process.env.DB_USER,// adicionando variaveis de ambiente no default.ts porque é um arquivo publico
-  dbPass: process.env.DB_PASS,
-  dbUri: `mongodb+srv://:@cluster0.6skx25o.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+  dbUri: `mongodb+srv://${dbUser}:${dbPass}@cluster0.6skx25o.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
 }
 
 // criando config para a porta da aplicação para não deixar hardcoded no app.ts
