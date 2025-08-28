7a308e8a434ee9ed9255d9b2cf47104688fadc24
Yrmih Ian <yrmihiangoncalvesdelima@gmail.com>
2025-08-28 11:53:53 -0300

    feat: adicionar arquivo .gitignore e atualizar dbUri para remover credenciais hardcoded

--- config/default.ts
+++ config/default.ts
@@ -1,7 +1,7 @@
 
 export default {
   port: 3000,
-  dbUri: 'mongodb+srv://<db_username>:<db_password>@cluster0.6skx25o.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+  dbUri: 'mongodb+srv://:@cluster0.6skx25o.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
 }
 
 // criando config para a porta da aplicação para não deixar hardcoded no app.ts
