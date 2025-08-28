54b803fd42620cf7d2d3b0e19fa7f6d4aac80fd1
Yrmih Ian <yrmihiangoncalvesdelima@gmail.com>
2025-08-27 23:29:07 -0300

    feat- criar app port from config

--- src/app.ts
+++ src/app.ts
@@ -6,6 +6,11 @@ const app = express();
 // JSON middleware
 app.use(express.json());
 
+// app port from config
+const port = config.get<number>('port'); // pegando a porta do config
+
+
+
 app.listen(3000, async () => {
   console.log('Aplicação está rodando na porta 3000'); // adicionando o app listen que serve para rodar a aplicação
 });
