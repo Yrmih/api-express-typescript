52fc746185e89e312ef212edd5715a27d7a60e51
Yrmih Ian <yrmihiangoncalvesdelima@gmail.com>
2025-08-28 02:04:23 -0300

    feat: criar arquivo de configuração do banco de dados

--- config/default.ts
+++ config/default.ts
@@ -1,5 +1,8 @@
+import db from "./db";
+
 export default {
   port: 3000,
+  dbUri: 'mongodb+srv://<db_username>:<db_password>@cluster0.6skx25o.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
 }
 
 // criando config para a porta da aplicação para não deixar hardcoded no app.ts
