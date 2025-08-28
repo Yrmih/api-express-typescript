06468b639e1dd1e1709ae7a47be91c3cc9c97f55
Yrmih Ian <yrmihiangoncalvesdelima@gmail.com>
2025-08-28 12:03:03 -0300

    feat- adicionando variaveis de ambiente no default.ts porque é um arquivo publico

--- config/default.ts
+++ config/default.ts
@@ -1,7 +1,9 @@
 
 export default {
   port: 3000,
-  dbUri: 'mongodb+srv://:@cluster0.6skx25o.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+  dbUser: process.env.DB_USER,
+  dbPass: process.env.DB_PASS,
+  dbUri: `mongodb+srv://:@cluster0.6skx25o.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
 }
 
 // criando config para a porta da aplicação para não deixar hardcoded no app.ts
