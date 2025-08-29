90d3c7b29a9f4fb9d0bd84291e89305a2be1455b
Yrmih Ian <yrmihiangoncalvesdelima@gmail.com>
2025-08-29 13:23:00 -0300

    feat: substituir console.log por Logger para melhor gerenciamento de logs na conexão com o MongoDB

--- config/db.ts
+++ config/db.ts
@@ -1,15 +1,16 @@
 import mongoose from "mongoose";
 import config from "config";
+import Logger from "./logger";
 
 async function connect() {
   const dbUri = config.get<string>("dbUri");
 
   try {
     await mongoose.connect(dbUri);
-    console.log("Conectado ao MongoDB");
+    Logger.info("Conectado ao MongoDB");
   } catch (error) {
-    console.log("Erro ao conectar ao MongoDB");
-    console.log(`${error}`);
+    Logger.info("Erro ao conectar ao MongoDB");
+    Logger.info(`${error}`);
   }
 }
 
