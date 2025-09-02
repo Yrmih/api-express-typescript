349ea28de73667edfce1a458462a6cb25b4ecbfa
Yrmih Ian <yrmihiangoncalvesdelima@gmail.com>
2025-09-02 02:08:34 -0300

    refactor: simplificar a extração de erros no middleware de validação

--- src/middleware/handleValidation.ts
+++ src/middleware/handleValidation.ts
@@ -1,22 +1,18 @@
-import { Request, Response, NextFunction } from 'express';
-import { validationResult, Result, ValidationError } from 'express-validator';
+import { Request, Response, NextFunction } from "express";
+import { validationResult } from "express-validator";
 
 export const validate = (req: Request, res: Response, next: NextFunction) => {
-  // Pega os erros da requisição
-  const errors: Result<ValidationError> = validationResult(req);
+  const errors = validationResult(req);
 
-  // Se não houver erros, segue para o próximo middleware
   if (errors.isEmpty()) {
     return next();
   }
+  const extractedErrors: object[] = [];
 
-  // Mapeia os erros para um formato mais amigável
-  const extractedErrors = errors.array().map(err => ({
-    [err.param]: err.msg
-  }));
+  // Força a tipagem para ValidationError
+  errors.array().map((err) => extractedErrors.push({ [err.param]: err.msg }));
 
-  // Retorna os erros
   return res.status(422).json({
-    errors: extractedErrors
+    errors: extractedErrors,
   });
 };
