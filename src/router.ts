f74cb4ca3971a20e09b590a15f0a941ca57561e1
Yrmih Ian <yrmihiangoncalvesdelima@gmail.com>
2025-09-02 02:11:44 -0300

    feat: adicionar middleware de validação na rota de criação de filme

--- src/router.ts
+++ src/router.ts
@@ -1,8 +1,12 @@
 import { Router, Request, Response} from "express";
 import { createMovie } from "./controllers/movieControllers";
 
+//Validation middleware
+import { validate } from "./middleware/handleValidation";
+
+
 const router = Router();
 
 export default router.get('/teste', (req: Request, res: Response) => {
   res.status(200).send('API funcionando!');
-}).post('/movie', createMovie);
+}).post('/movie', validate, createMovie);
