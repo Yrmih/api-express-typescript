3a967c205ba8df0fdfc6e244726331c0063d8d8a
Yrmih Ian <yrmihiangoncalvesdelima@gmail.com>
2025-09-01 22:58:41 -0300

    feat: adicionar rota para criação de filmes no roteador

--- src/router.ts
+++ src/router.ts
@@ -1,7 +1,8 @@
 import { Router, Request, Response} from "express";
+import { createMovie } from "./controllers/movieControllers";
 
 const router = Router();
 
 export default router.get('/teste', (req: Request, res: Response) => {
   res.status(200).send('API funcionando!');
-});
+}).post('/movie', createMovie);
