9f251d451c9e754a81e9b133fd1dd8aaaa43e479
Yrmih Ian <yrmihiangoncalvesdelima@gmail.com>
2025-09-05 00:48:52 -0300

    feat: adicionar validação para criação de filme no middleware

--- src/middleware/handleValidation.ts
+++ src/middleware/handleValidation.ts
@@ -16,3 +16,8 @@ export const validate = (req: Request, res: Response, next: NextFunction) => {
     errors: extractedErrors,
   });
 };
+
+// o validate é um middleware que vai ser usado nas rotas para validar os dados que estão vindo no corpo da requisição
+// ele usa o express-validator para fazer a validação
+// se tiver algum erro ele retorna um array de erros com a mensagem de cada erro
+// se não tiver erro ele chama o next() para continuar o fluxo da aplicação
