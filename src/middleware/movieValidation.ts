d52388c14aa6acbdd32f1002cfc396872e87980b
Yrmih Ian <yrmihiangoncalvesdelima@gmail.com>
2025-09-05 01:42:52 -0300

    feat: adicionar validação para o campo descrição na criação de filmes

--- src/middleware/movieValidation.ts
+++ src/middleware/movieValidation.ts
@@ -14,6 +14,7 @@ export const movieCreateValidation = () => {
         }
         return true;
       }),
+      body("description").isString().withMessage("A descrição é obrigatória."),
   ];
 };
 
